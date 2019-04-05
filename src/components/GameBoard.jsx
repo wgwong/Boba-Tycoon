fce02b913330c903900bc1be0147ab146db18545
Will Wong <wgwong17@gmail.com>
2019-04-05 10:20:50 -0700

    skeleton

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './GameBoard.css';
+
+class GameBoard extends Component {
+	render() {
+		return (
+			<div className="GameBoard">
+				
+			</div>
+		);
+	}
+}
+
+export default GameBoard;
